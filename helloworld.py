3c38c90c68d86751dac623e0d1aa7ba9094314b1
Dean Silfen <dsilfen@handy.com>
2016-03-21 20:50:07 -0400

    update helloworld

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
-puts 'Hello World'
-puts 'How is your day going?'
-puts 'its warm inside this machine'
-puts "Where do you think you're going?"
+print('Hello World')
+print('How is your day going?')
+print('its warm inside this machine')
+print("Where do you think you're going?")
 
